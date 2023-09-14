d7b8f5d1ac48879709f0cdd4269ddc7fc88d2f22
Kelvin Koh <there.are.no.mails@gmail.com>
2023-09-14 19:55:50 +0800

    Initial release

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Simple utility to download S3 Object with standard AWS Credentials resolution.
 ## Installation:
 
 ```sh
-$ go get github.com/stupoid/s3-get
+$ go install github.com/stupoid/s3-get
 ```
 
 ## Usage:
@@ -13,16 +13,39 @@ $ go get github.com/stupoid/s3-get
 ```sh
 $ ./s3-get -h
 Usage of ./s3-get:
+  -concurrency int
+    	The number of goroutines to spin up in parallel when sending parts.
+    	If this is set to zero, the DefaultDownloadConcurrency value will be used.
+
+    	Concurrency of 1 will download the parts sequentially.
+
+    	Concurrency is ignored if the Range input parameter is provided.
   -dst string
-        Destination output path.
+    	Destination output path.
   -endpoint string
-        Optional custom endpoint to use (for connecting to S3 compatible interfaces).
-        Alternatively this can be set via environment variable 'AWS_ENDPOINT_URL'.
-        But this flag will take precedence.
+    	AWS endpoint to use (for connecting to S3 compatible interfaces).
+    	Alternatively this can be set via environment variable 'S3_GET_ENDPOINT'.
+
+    	If this is not set, the default AWS service endpoint will be used.
+
+    	The value set by this flag will take precedence over the environment variable.
+  -part-size int
+    	The size (in bytes) to request from S3 for each part.
+    	The minimum allowed part size is 5MB, and  if this value is set to zero,
+    	the DefaultDownloadPartSize value will be used.
+
+    	PartSize is ignored if the Range input parameter is provided.
+  -quiet
+    	Silence error output. (default true)
   -src string
-        Source S3 URI.
+    	Source S3 URI.
   -timeout duration
-        Download timeout. (default 1m0s)
+    	Download timeout.
+    	If this is set to zero, no timeout will be set.
+
+    	A duration string is a possibly signed sequence of decimal numbers,
+    	each with optional fraction and a unit suffix, such as "300ms", "-1.5h" or "2h45m".
+    	Valid time units are "ns", "us" (or "µs"), "ms", "s", "m", "h".
 ```
 
 ### Example usage
@@ -36,12 +59,16 @@ $ ./s3-get -src=s3://my-bucket/credentials/private.crt -dst=./cert.pem
 Usage with local mock S3 server
 
 ```sh
+# start up a local mock s3 server (e.g. localstack)
+$ docker --rm -d run -p 4566:4566 localstack/localstack -e SERVICES s3
+```
+
+```
 $ ./s3-get -src=s3://my-bucket/credentials/private.crt -dst=./cert.pem -endpoint=http://127.0.0.1:4566
 ```
 
 Alternate way to set custom aws endpoint
 
 ```sh
-$ export AWS_ENDPOINT_URL=http://127.0.0.1:4566
-$ ./s3-get -src=s3://my-bucket/credentials/private.crt -dst=./cert.pem
+$ S3_GET_ENDPOINT=http://127.0.0.1:4566 ./s3-get -src=s3://my-bucket/credentials/private.crt -dst=./cert.pem
 ```
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net/url"
 	"os"
@@ -18,21 +19,19 @@ import (
 
 // newS3Client returns a s3.Client with configurable endpoint
 // If the endpoint provided is an empty string, it will default to AWS
-func newS3Client(endpoint string) (client *s3.Client, err error) {
+func newS3Client(endpoint string) (*s3.Client, error) {
 	cfg, err := config.LoadDefaultConfig(context.TODO())
 	if err != nil {
-		return
+		return nil, err
 	}
 
-	if endpoint != "" {
-		log.Printf("s3.Client using endpoint %v\n", endpoint)
-		client = s3.NewFromConfig(cfg, func(o *s3.Options) {
+	client := s3.NewFromConfig(cfg, func(o *s3.Options) {
+		if endpoint != "" {
 			o.BaseEndpoint = aws.String(endpoint)
-		})
-	} else {
-		client = s3.NewFromConfig(cfg)
-	}
-	return
+		}
+	})
+
+	return client, nil
 }
 
 // ParseS3URI extracts the bucket and object key from a S3 URI
@@ -40,81 +39,119 @@ func newS3Client(endpoint string) (client *s3.Client, err error) {
 func ParseS3URI(s3URI string) (bucket, key string, err error) {
 	u, err := url.Parse(s3URI)
 	if err != nil {
-		return
+		return "", "", err
 	}
 	if u.Scheme != "s3" {
-		err = fmt.Errorf("invalid scheme %v", u.Scheme)
-		return
+		return "", "", fmt.Errorf("invalid scheme %v", u.Scheme)
 	}
 	bucket = u.Host
 	key = strings.TrimLeft(u.Path, "/")
-	return
+	return bucket, key, nil
 }
 
 // Download retrieves an object from S3 and writes it to a file.
-func Download(c *s3.Client, timeout time.Duration, bucket, key, fileName string) error {
+func Download(d *manager.Downloader, timeout time.Duration, bucket, key, fileName string) (int64, error) {
 	ctx := context.TODO()
-	ctx, cancel := context.WithTimeout(ctx, timeout)
-	defer cancel()
+	var cancel context.CancelFunc
+
+	if timeout != 0 {
+		ctx, cancel = context.WithTimeout(ctx, timeout)
+		defer cancel()
+	}
 
 	file, err := os.Create(fileName)
 	if err != nil {
-		return err
+		return 0, err
 	}
 	defer file.Close()
 
-	d := manager.NewDownloader(c)
-	_, err = d.Download(ctx, file, &s3.GetObjectInput{
+	bytesDownloaded, err := d.Download(ctx, file, &s3.GetObjectInput{
 		Bucket: aws.String(bucket),
 		Key:    aws.String(key),
 	})
 	if err != nil {
 		os.Remove(fileName) // cleanup empty file if fail to get Object
-		return err
+		return 0, err
 	}
-	return nil
+	return bytesDownloaded, nil
 }
 
 func main() {
-	var s3URI, fileName, AWSEndpointURL string
+	var s3URI, fileName, endpoint string
 	var timeout time.Duration
+	var quiet bool
+	var partSize int64
+	var concurrency int
 
 	flag.StringVar(&s3URI, "src", "", "Source S3 URI.")
 	flag.StringVar(&fileName, "dst", "", "Destination output path.")
-	flag.StringVar(&AWSEndpointURL, "endpoint", "", `Optional custom endpoint to use (for connecting to S3 compatible interfaces).
-Alternatively this can be set via environment variable 'AWS_ENDPOINT_URL'.
-But this flag will take precedence.`,
+	flag.StringVar(&endpoint, "endpoint", "", `AWS endpoint to use (for connecting to S3 compatible interfaces).
+Alternatively this can be set via environment variable 'S3_GET_ENDPOINT'.
+
+If this is not set, the default AWS service endpoint will be used.
+
+The value set by this flag will take precedence over the environment variable.`,
 	)
-	flag.DurationVar(&timeout, "timeout", time.Minute, "Download timeout.")
+	flag.DurationVar(&timeout, "timeout", 0, `Download timeout.
+If this is set to zero, no timeout will be set.
+
+A duration string is a possibly signed sequence of decimal numbers,
+each with optional fraction and a unit suffix, such as "300ms", "-1.5h" or "2h45m".
+Valid time units are "ns", "us" (or "µs"), "ms", "s", "m", "h".
+	`)
+	flag.BoolVar(&quiet, "quiet", true, "Silence error output.")
+	flag.Int64Var(&partSize, "part-size", 0, `The size (in bytes) to request from S3 for each part.
+The minimum allowed part size is 5MB, and  if this value is set to zero,
+the DefaultDownloadPartSize value will be used.
+
+PartSize is ignored if the Range input parameter is provided.`)
+	flag.IntVar(&concurrency, "concurrency", 0, `The number of goroutines to spin up in parallel when sending parts.
+If this is set to zero, the DefaultDownloadConcurrency value will be used.
+
+Concurrency of 1 will download the parts sequentially.
+
+Concurrency is ignored if the Range input parameter is provided.`)
 	flag.Parse()
 
+	logger := log.New(os.Stdout, "", 0)
+
+	if quiet {
+		logger.SetOutput(io.Discard)
+	}
+
 	if s3URI == "" {
-		log.Fatal("Source S3 URI not set")
+		logger.Fatal("source S3 URI not set")
 	}
 
 	if fileName == "" {
-		log.Fatal("Destination output path not set")
+		logger.Fatal("destination output path not set")
 	}
 
 	bucket, key, err := ParseS3URI(s3URI)
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal(err)
 	}
 
 	if _, err = os.Stat(fileName); !os.IsNotExist(err) {
-		log.Fatalf("File already exists: %s\n", fileName)
+		logger.Fatalf("file already exists: %s\n", fileName)
 	}
 
-	if AWSEndpointURL == "" {
-		AWSEndpointURL = os.Getenv("AWS_ENDPOINT_URL")
+	if endpoint == "" {
+		endpoint = os.Getenv("S3_GET_ENDPOINT")
 	}
 
-	client, err := newS3Client(AWSEndpointURL)
+	client, err := newS3Client(endpoint)
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal(err)
 	}
 
-	if err := Download(client, timeout, bucket, key, fileName); err != nil {
-		log.Fatal(err)
+	downloader := manager.NewDownloader(client, func(d *manager.Downloader) {
+		d.PartSize = partSize
+		d.Concurrency = concurrency
+	})
+	bytesDownloaded, err := Download(downloader, timeout, bucket, key, fileName)
+	if err != nil {
+		logger.Fatal(err)
 	}
+	logger.Printf("downloaded %dB: %v to %v\n", bytesDownloaded, s3URI, fileName)
 }
